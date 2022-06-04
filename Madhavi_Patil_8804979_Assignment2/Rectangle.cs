d7496d8b9f8cb4158df0da47462c6b7c6509fe3d
Madhavi aashwin Patil <mpatil4979@conestogac.on.ca>
2022-06-04 19:28:36 -0400

    Rectangle class completed

---
+++ Madhavi_Patil_8804979_Assignment2/Rectangle.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Madhavi_Patil_8804979_Assignment2
+{
+    public class Rectangle
+    {
+
+        private int length;
+        private int width;
+
+        //default constructor
+        Rectangle()
+        {
+            length = 1;
+            width = 1;
+        }
+
+        //non-default constructor
+        Rectangle(int x, int y)
+        {
+            length = x;
+            width = y;
+        }
+
+        //methods
+        public int GetLength()
+        {
+            return length;
+        }
+
+        public int SetLength(int length)
+        {
+            this.length = length;
+            return length;
+        }
+
+        public int GetWidth()
+        {
+            return width;
+        }
+
+        public int SetWidth(int width)
+        {
+            this.width = width;
+            return width;
+        }
+
+        public int GetPerimeter()
+        {
+            int perimeter = 2*(length*width);  
+            return perimeter;
+        }
+
+        public int GetArea()
+        {
+            int area = length*width;
+            return area;
+        }
+
+    }
+}
