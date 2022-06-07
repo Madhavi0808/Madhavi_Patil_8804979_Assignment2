5f0f9d119e0b47d828c262263d4b1d3011470d85
Madhavi aashwin Patil <mpatil4979@conestogac.on.ca>
2022-06-06 22:08:59 -0400

    containing the final completed unit tests

--- Madhavi_Patil_8804979_Assignment2Tests/RectangleTest.cs
+++ Madhavi_Patil_8804979_Assignment2Tests/RectangleTest.cs
@@ -16,59 +16,44 @@ namespace Madhavi_Patil_8804979_Assignment2Tests
         public class RectangleTest1
         {
             [Test]
-
-
-            public void RectangleLength_Input3()
+            public void GetRectangleLength_Input3()
             {
                 Rectangle rectangle = new Rectangle(3,6);
                 int expected = 3;
                 int actual = rectangle.GetLength();
                 Assert.AreEqual(expected, actual);
             }
-        }
-        [TestFixture]
-        public class RectangleTest2
-        {
+
             [Test]
-
-
-            public void RectangleLength_Input7()
+            public void GetRectangleLength_Input7()
             {
                 Rectangle rectangle = new Rectangle(7, 10);
                 int expected = 7;
                 int actual = rectangle.GetLength();
                 Assert.AreEqual(expected, actual);
             }
-        }
-        public class RectangleTest3
-        {
+
             [Test]
-
-
-            public void RectangleLength_Input2()
+            public void GetRectangleLength_Input2()
             {
                 Rectangle rectangle = new Rectangle(2, 5);
                 int expected = 2;
                 int actual = rectangle.GetLength();
                 Assert.AreEqual(expected, actual);
             }
-        }
-        public class RectangleTest4
-        {
+
             [Test]
-            public void RectangleLength_Input5()
+            public void SetRectangleLength_Input5()
             {
                 Rectangle rectangle = new Rectangle(5, 10);
-                
+
                 int actual = rectangle.SetLength(5);
                 int expected = 5;
                 Assert.AreEqual(expected, actual);
             }
-        }
-        public class RectangleTest5
-        {
+
             [Test]
-            public void RectangleLength_Input2()
+            public void SetRectangleLength_Input2()
             {
                 Rectangle rectangle = new Rectangle(2, 3);
 
@@ -76,11 +61,9 @@ namespace Madhavi_Patil_8804979_Assignment2Tests
                 int expected = 2;
                 Assert.AreEqual(expected, actual);
             }
-        }
-        public class RectangleTest6
-        {
+
             [Test]
-            public void RectangleLength_Input10()
+            public void SetRectangleLength_Input10()
             {
                 Rectangle rectangle = new Rectangle(10, 30);
 
@@ -88,50 +71,36 @@ namespace Madhavi_Patil_8804979_Assignment2Tests
                 int expected = 10;
                 Assert.AreEqual(expected, actual);
             }
-        }
-        public class RectangleTest7
-        {
+
             [Test]
-
-
-            public void RectangleWidth_Input6()
+            public void GetRectangleWidth_Input6()
             {
                 Rectangle rectangle = new Rectangle(3, 6);
                 int expected = 6;
                 int actual = rectangle.GetWidth();
                 Assert.AreEqual(expected, actual);
             }
-        }
-        public class RectangleTest8
-        {
+
             [Test]
-
-
-            public void RectangleWidth_Input10()
+            public void GetRectangleWidth_Input10()
             {
                 Rectangle rectangle = new Rectangle(7, 10);
                 int expected = 10;
                 int actual = rectangle.GetWidth();
                 Assert.AreEqual(expected, actual);
             }
-        }
-        public class RectangleTest9
-        {
+
             [Test]
-
-
-            public void RectangleWidth_Input5()
+            public void GetRectangleWidth_Input5()
             {
                 Rectangle rectangle = new Rectangle(2, 5);
                 int expected = 5;
                 int actual = rectangle.GetWidth();
                 Assert.AreEqual(expected, actual);
             }
-        }
-        public class RectangleTest10
-        {
+
             [Test]
-            public void RectangleWidth_Input10()
+            public void SetRectangleWidth_Input10()
             {
                 Rectangle rectangle = new Rectangle(5, 10);
 
@@ -139,11 +108,9 @@ namespace Madhavi_Patil_8804979_Assignment2Tests
                 int expected = 10;
                 Assert.AreEqual(expected, actual);
             }
-        }
-        public class RectangleTest11
-        {
+
             [Test]
-            public void RectangleWidth_Input3()
+            public void SetRectangleWidth_Input3()
             {
                 Rectangle rectangle = new Rectangle(2, 3);
 
@@ -151,11 +118,9 @@ namespace Madhavi_Patil_8804979_Assignment2Tests
                 int expected = 3;
                 Assert.AreEqual(expected, actual);
             }
-        }
-        public class RectangleTest12
-        {
+
             [Test]
-            public void RectangleWidth_Input10()
+            public void SetRectangleWidth_Input30()
             {
                 Rectangle rectangle = new Rectangle(10, 30);
 
@@ -163,80 +128,60 @@ namespace Madhavi_Patil_8804979_Assignment2Tests
                 int expected = 30;
                 Assert.AreEqual(expected, actual);
             }
-        }
-        public class RectangleTest13
-        {
+
             [Test]
-            public void RectanglePerimeter()
+            public void GetRectanglePerimeter_7and10()
             {
                 Rectangle rectangle = new Rectangle(7, 10);
                 int expected = 34;
                 int actual = rectangle.GetPerimeter();
                 Assert.AreEqual(expected, actual);
             }
-        }
-        public class RectangleTest14
-        {
+
             [Test]
-
-
-            public void RectanglePerimeter()
+            public void GetRectanglePerimeter_3and6()
             {
                 Rectangle rectangle = new Rectangle(3, 6);
                 int expected = 18;
                 int actual = rectangle.GetPerimeter();
                 Assert.AreEqual(expected, actual);
             }
-        }
-        public class RectangleTest15
-        {
+
             [Test]
-
-
-            public void RectanglePerimeter()
+            public void GetRectanglePerimeter_2and5()
             {
                 Rectangle rectangle = new Rectangle(2, 5);
                 int expected = 14;
                 int actual = rectangle.GetPerimeter();
                 Assert.AreEqual(expected, actual);
             }
-        }
-        public class RectangleTest16
-        {
+
             [Test]
-            public void RectangleArea()
+            public void GetRectangleArea_7and10()
             {
                 Rectangle rectangle = new Rectangle(7, 10);
                 int expected = 70;
                 int actual = rectangle.GetArea();
                 Assert.AreEqual(expected, actual);
             }
-        }
-        public class RectangleTest17
-        {
+
             [Test]
-
-
-            public void RectangleArea()
+            public void GetRectangleArea_3and6()
             {
                 Rectangle rectangle = new Rectangle(3, 6);
                 int expected = 18;
                 int actual = rectangle.GetArea();
                 Assert.AreEqual(expected, actual);
             }
-        }
-        public class RectangleTest18
-        {
             [Test]
-
-
-            public void RectangleArea()
+            public void GetRectangleArea_2and5()
             {
                 Rectangle rectangle = new Rectangle(2, 5);
                 int expected = 10;
                 int actual = rectangle.GetArea();
                 Assert.AreEqual(expected, actual);
             }
+
         }
     }
 }
